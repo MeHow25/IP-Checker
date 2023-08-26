b7d884269b9dea16302b83c41936eddc85f84771
michal <sadsad@fsad.pl>
2023-08-26 18:34:23 +0200

    Show raw response

--- index.php
+++ index.php
@@ -1,6 +1,21 @@
 <?php
 
-echo 'hello world';
+$curl = curl_init();
 
+$userIp = $_SERVER['REMOTE_ADDR'];
 
+curl_setopt_array($curl, [
+  CURLOPT_URL => "https://freeipapi.com/api/json/".$userIp,
+  CURLOPT_RETURNTRANSFER => true
+]);
 
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo '<pre>'.$response.'</pre>';
+}
